122a9d4794dc0ea938d2321058b10f6612c1f5cf
pmw57 <paul.m.wilkins@gmail.com>
2019-05-28 21:29:52 +1200

    aircraftCalcs only needs to know values are occurring

--- test/aircraftCalculationsTest.js
+++ test/aircraftCalculationsTest.js
@@ -4,28 +4,14 @@ import {assert} from "chai";
 import aircraftCalcs from "../src/aircraftCalculations.js";
 
 describe("Output test", function () {
-    it("has a sealevel wing load", function () {
-        var output = aircraftCalcs.outputs({
-            altitude_ft: 0,
-            cl_max_clean: 1.53,
-            vs1: 67
-        });
-        assert.closeTo(output.wing_load_lb_ft, 17.57, 0.01);
-    });
-    it("changes wing load with sealevel", function () {
-        var output = aircraftCalcs.outputs({
-            altitude_ft: 10000,
-            cl_max_clean: 1.53,
-            vs1: 67
-        });
-        assert.closeTo(output.wing_load_lb_ft, 12.97, 0.01);
-    });
     it("has same wing load at faster speed", function () {
         var output = aircraftCalcs.outputs({
-            altitude_ft: 10000,
+            vs1: 77.95,
             cl_max_clean: 1.53,
-            vs1: 77.95
+            cl_max_flap: 2.1,
+            altitude_ft: 10000
         });
-        assert.closeTo(output.wing_load_lb_ft, 17.57, 0.01);
+        assert.isNumber(output.wing_load_lb_ft);
+        assert.isNumber(output.vs0);
     });
 });
