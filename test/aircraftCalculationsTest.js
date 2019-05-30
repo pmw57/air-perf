a36fb853c890b8688f43356095ee82592b9740b0
pmw57 <paul.m.wilkins@gmail.com>
2019-05-30 23:22:21 +1200

    Results tests

--- test/aircraftCalculationsTest.js
+++ test/aircraftCalculationsTest.js
@@ -1,10 +1,10 @@
 /*jslint browser */
-import {describe, it, xit} from "mocha";
+import {beforeEach, describe, it} from "mocha";
 import {assert} from "chai";
 import aircraftCalcs from "../src/aircraftCalculations.js";
 
-describe("Output test", function () {
-    var output = aircraftCalcs.outputs({
+describe("calculation tests", function () {
+    const inputs = {
         vs1: 77.95,
         cl_max_clean: 1.53,
         cl_max_flap: 2.1,
@@ -17,70 +17,93 @@ describe("Output test", function () {
         prop_dia_in: 72,
         prop_max_rpm: 2700,
         altitude_ft: 10000
+    };
+    describe("Output test", function () {
+        const output = aircraftCalcs.outputs(inputs);
+        it("has a wing load value", function () {
+            assert.isFinite(output.wing_load_lb_ft);
+        });
+        it("has a landing configuration stall velocity", function () {
+            assert.isFinite(output.vs0);
+        });
+        it("has a wing area", function () {
+            assert.isFinite(output.wing_area_ft);
+        });
+        it("has a wing aspect", function () {
+            assert.isFinite(output.wing_aspect);
+        });
+        it("has a wing chord", function () {
+            assert.isFinite(output.wing_chord_ft);
+        });
+        it("has an effective wing span", function () {
+            assert.isFinite(output.wing_span_effective);
+        });
+        it("has an effective wing chord", function () {
+            assert.isFinite(output.wing_chord_effective);
+        });
+        it("has an effective span load", function () {
+            assert.isFinite(output.span_load_effective);
+        });
+        it("has a drag area", function () {
+            assert.isFinite(output.drag_area_ft);
+        });
+        it("has a zerolift drag coefficient", function () {
+            assert.isFinite(output.zerolift_drag_coefficient);
+        });
+        it("has a min sink velocity", function () {
+            assert.isFinite(output.vel_sink_min_ft);
+        });
+        it("has a min required power", function () {
+            assert.isFinite(output.pwr_min_req_hp);
+        });
+        it("has a min sink rate", function () {
+            assert.isFinite(output.rate_sink_min_ft);
+        });
+        it("has a max lift/drag ratio", function () {
+            assert.isFinite(output.ld_max);
+        });
+        it("has a minimum drag", function () {
+            assert.isFinite(output.drag_min);
+        });
+        it("has a min coefficient of lift", function () {
+            assert.isFinite(output.cl_min_sink);
+        });
+        it("has an ideal rate of climb", function () {
+            assert.isFinite(output.rate_climb_ideal);
+        });
+        it("has a prop tip mach", function () {
+            assert.isFinite(output.prop_tip_mach);
+        });
+        it("has a prop velocity", function () {
+            assert.isFinite(output.prop_vel_ref);
+        });
+        it("has an ideal static thrust", function () {
+            assert.isFinite(output.static_thrust_ideal);
+        });
     });
-    it("has a wing load value", function () {
-        assert.isFinite(output.wing_load_lb_ft);
-    });
-    it("has a landing configuration stall velocity", function () {
-        assert.isFinite(output.vs0);
-    });
-    it("has a wing area", function () {
-        assert.isFinite(output.wing_area_ft);
-    });
-    it("has a wing aspect", function () {
-        assert.isFinite(output.wing_aspect);
-    });
-    it("has a wing chord", function () {
-        assert.isFinite(output.wing_chord_ft);
-    });
-    it("has an effective wing span", function () {
-        assert.isFinite(output.wing_span_effective);
-    });
-    it("has an effective wing chord", function () {
-        assert.isFinite(output.wing_chord_effective);
-    });
-    it("has an effective span load", function () {
-        assert.isFinite(output.span_load_effective);
-    });
-    it("has a drag area", function () {
-        assert.isFinite(output.drag_area_ft);
-    });
-    it("has a zerolift drag coefficient", function () {
-        assert.isFinite(output.zerolift_drag_coefficient);
-    });
-    it("has a min sink velocity", function () {
-        assert.isFinite(output.vel_sink_min_ft);
-    });
-    it("has a min required power", function () {
-        assert.isFinite(output.pwr_min_req_hp);
-    });
-    it("has a min sink rate", function () {
-        assert.isFinite(output.rate_sink_min_ft);
-    });
-    it("has a max lift/drag ratio", function () {
-        assert.isFinite(output.ld_max);
-    });
-    it("has a minimum drag", function () {
-        assert.isFinite(output.drag_min);
-    });
-    it("has a min coefficient of lift", function () {
-        assert.isFinite(output.cl_min_sink);
-    });
-    it("has an ideal rate of climb", function () {
-        assert.isFinite(output.rate_climb_ideal);
-    });
-    it("has a prop tip mach", function () {
-        assert.isFinite(output.prop_tip_mach);
-    });
-    it("has a prop velocity", function () {
-        assert.isFinite(output.prop_vel_ref);
-    });
-    it("has an ideal static thrust", function () {
-        assert.isFinite(output.static_thrust_ideal);
-    });
-});
-describe("Results test", function () {
-    xit("", function () {
-        return;
+    describe("Results test", function () {
+        const outputs = aircraftCalcs.outputs(inputs);
+        let results;
+        beforeEach(function () {
+            results = aircraftCalcs.results(inputs, outputs);
+        });
+        it("has performance parameter", function () {
+            assert.closeTo(results.fp, 0.14, 0.01);
+        });
+        it("has max rate of climb", function () {
+            assert.closeTo(results.rcmax, 1641, 1);
+        });
+        it("has useful load", function () {
+            assert.equal(results.useful_load, 600);
+        });
+        it("has max velocity", function () {
+            assert.closeTo(results.vmax, 227, 0.1);
+        });
+        it("has kinetic energy", function () {
+            assert.closeTo(results.wv2, 78000000, 100000);
+        });
+        it("has data", function () {
+            assert.isAbove(results.data.length, 10);
+        });
     });
 });
