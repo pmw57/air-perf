7f31260dd3f9c59fbe296107272f1fee2e2d9941
Paul Wilkins <paul.m.wilkins@gmail.com>
2019-06-04 12:39:59 +1200

    Move input tests into their own describe section

--- test/aircraftViewTest.js
+++ test/aircraftViewTest.js
@@ -6,20 +6,22 @@ import view from "../src/aircraftView.js";
 describe("Air performance test", function () {
     var outputs = {};
     var results = {};
-    it("updates an input", function () {
-        var inputs = {vs1: "67"};
-        var form = {elements: {vs1: {value: ""}}};
-        view.init(form);
-        view.renderInputs(inputs);
-        assert.equal(inputs.vs1, form.elements.vs1.value);
-    });
-    it("throws an error with a non-matching input", function () {
-        var inputs = {shouldntmatch: "67"};
-        var form = {elements: {vs1: {value: ""}}};
-        view.init(form);
-        assert.throws(function () {
+    describe("inputs", function () {
+        it("updates an input", function () {
+            const inputs = {vs1: "67"};
+            const form = {elements: {vs1: {value: ""}}};
+            view.init(form);
             view.renderInputs(inputs);
-        }, ReferenceError);
+            assert.equal(inputs.vs1, form.elements.vs1.value);
+        });
+        it("throws an error with a non-matching input", function () {
+            const inputs = {shouldntmatch: "67"};
+            const form = {elements: {vs1: {value: ""}}};
+            view.init(form);
+            assert.throws(function () {
+                view.renderInputs(inputs);
+            }, ReferenceError);
+        });
     });
     it.skip("updates an output", function () {
         return;
