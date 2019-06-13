0959d5e3a2d3b1890bd00a332723b75be6c894ea
Paul Wilkins <paul.m.wilkins@gmail.com>
2019-06-14 10:09:34 +1200

    Reduce view test nesting

--- test/aircraftViewTest.js
+++ test/aircraftViewTest.js
@@ -24,46 +24,40 @@ describe("Aircraft view tests", function () {
             }
         };
     });
-    describe("inputs", function () {
-        it("updates an input", function () {
-            const inputs = {vs1: "67"};
-            const form = {elements: {vs1: {value: ""}}};
-            view.init(form);
-            view.renderInputs(inputs);
-            assert.equal(inputs.vs1, form.elements.vs1.value);
-        });
+    it("updates an input", function () {
+        const inputs = {vs1: "67"};
+        const form = {elements: {vs1: {value: ""}}};
+        view.init(form);
+        view.renderInputs(inputs);
+        assert.equal(inputs.vs1, form.elements.vs1.value);
     });
-    describe("outputs", function () {
-        it("updates an output", function () {
-            els = {
-                "#vs0": newElement(),
-                "#summary": doc
-            };
-            const vs0 = 57.19;
-            const outputs = {vs0};
-            const precision = {vs0: 2};
-            view.init(undefined, doc);
-            view.renderOutputs(outputs, precision);
-            assert.equal(els["#vs0"].innerHTML, vs0);
-        });
+    it("updates an output", function () {
+        els = {
+            "#vs0": newElement(),
+            "#summary": doc
+        };
+        const vs0 = 57.19;
+        const outputs = {vs0};
+        const precision = {vs0: 2};
+        view.init(undefined, doc);
+        view.renderOutputs(outputs, precision);
+        assert.equal(els["#vs0"].innerHTML, vs0);
     });
-    describe("results", function () {
-        it("updates a result", function () {
-            const results = {
-                key1: 3,
-                key2: 4
-            };
-            const table = [];
-            els = {
-                tbody: {},
-                "#key1": newElement(),
-                "#key2": newElement()
-            };
-            const precision = {};
-            view.init(undefined, doc);
-            view.renderResults({results, table}, precision);
-            assert.equal(els["#key1"].innerHTML, "3");
-            assert.equal(els["#key2"].innerHTML, "4");
-        });
+    it("updates a result", function () {
+        const results = {
+            key1: 3,
+            key2: 4
+        };
+        const table = [];
+        els = {
+            tbody: {},
+            "#key1": newElement(),
+            "#key2": newElement()
+        };
+        const precision = {};
+        view.init(undefined, doc);
+        view.renderResults({results, table}, precision);
+        assert.equal(els["#key1"].innerHTML, "3");
+        assert.equal(els["#key2"].innerHTML, "4");
     });
 });
