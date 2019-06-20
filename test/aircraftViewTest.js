427a593bede0d43c4604376dbdc4a3c8f15ceede
Paul Wilkins <paul.m.wilkins@gmail.com>
2019-06-20 12:36:08 +1200

    Use jsdom for view tests

--- test/aircraftViewTest.js
+++ test/aircraftViewTest.js
@@ -1,124 +1,121 @@
 import {describe, beforeEach, it} from "mocha";
 import {assert} from "chai";
 import view from "../src/aircraftView.js";
+import {JSDOM} from "jsdom";
 
 describe("Aircraft view tests", function () {
-    let els;
-    let doc;
+    const dom = new JSDOM("<form>" +
+            "    <input name=vs1 value=67>" +
+            "    <input name=cl_max_clean value=1.53>" +
+            "</form>" +
+            "<table id=summary><tr>" +
+            "    <td id=vs0>57.19</td>" +
+            "    <td id=wing_area_ft>85.39</td>" +
+            "    <td id=wing_aspect>5.08</td>" +
+            "</tr></table>" +
+            "<div id=results>" +
+            "  <ul>" +
+            "    <li><span id=fp>0.1207</span></li>" +
+            "    <li><span id=wv2>45414000</span></li>" +
+            "  </ul>" +
+            "  <div id=summary>" +
+            "    <table>" +
+            "      <tbody>" +
+            "      </tbody>" +
+            "    </table>" +
+            "  </div>" +
+            "</div>");
+    const document = dom.window.document;
+
     let form;
-    function newElement() {
-        return {value: ""};
-    }
     beforeEach(function () {
-        doc = {
-            querySelector: function querySelector(selector) {
-                if (selector === "#vs0") {
-                    return els[selector];
-                }
-                if (selector === "#results") {
-                    return {querySelector};
-                }
-                if (selector === "table") {
-                    return {tBodies: [els.tbody]};
-                }
-                return els[selector];
-            }
-        };
+        form = document.querySelector("form");
     });
     it("gets elements from the form", function () {
-        form = {
-            elements: {
-                "vs1": "67",
-                "cl_max_clean": "1.53"
-            }
-        };
-        view.init(form);
+        view.init(document.querySelector("form"));
         const elements = view.getFormElements();
-        assert.equal(elements.vs1, "67");
-        assert.equal(elements.cl_max_clean, "1.53");
+        assert.equal(elements[0].name, "vs1");
+        assert.equal(elements[0].value, "67");
+        assert.equal(elements[1].name, "cl_max_clean");
+        assert.equal(elements[1].value, "1.53");
     });
     it("gets values of input fields", function () {
-        form = {elements: [
-            {name: "vs1", value: "67"},
-            {name: "cl_max_clean", value: "1.53"}
-        ]};
-        view.init(form);
+        view.init(document.querySelector("form"));
         const values = view.getFormValues();
         assert.equal(values.vs1, "67");
         assert.equal(values.cl_max_clean, "1.53");
     });
+    it.skip("throws an error when no summary exists", function () {
+        return;
+    });
     it("gets output fields", function () {
-        const fakeDoc = {
-            querySelector: function () {
-                return {
-                    querySelectorAll: function querySelector() {
-                        return [
-                            {id: "field1", textContent: "123"},
-                            {id: "field2", textContent: "456"},
-                            {id: "field3", textContent: "789"}
-                        ];
-                    }
-                };
-            }
-        };
-        view.init(null, fakeDoc);
+        view.init(form, document);
         const outputFields = view.getOutputFields();
-        assert.equal(outputFields.field1, "123");
-        assert.equal(outputFields.field2, "456");
-        assert.equal(outputFields.field3, "789");
+        assert.equal(outputFields.vs0, "57.19");
+        assert.equal(outputFields.wing_area_ft, "85.39");
+        assert.equal(outputFields.wing_aspect, "5.08");
+    });
+    it.skip("throws an error when no results exist", function () {
+        return;
     });
     it("gets results fields", function () {
-        const fakeDoc = {
-            querySelector: function () {
-                return {
-                    querySelectorAll: function () {
-                        return [
-                            {id: "vs1", textContent: "67"},
-                            {id: "cl_max_clean", textContent: "1.53"}
-                        ];
-                    }
-                };
-            }
-        };
-        view.init(null, fakeDoc);
+        view.init(form, document);
         const resultFields = view.getResultFields();
-        assert.equal(resultFields.vs1, "67");
-        assert.equal(resultFields.cl_max_clean, "1.53");
+        assert.equal(resultFields.fp, "0.1207");
+        assert.equal(resultFields.wv2, "45414000");
+    });
+    it.skip("throws an error when no form exists", function () {
+        return;
     });
     it("updates an input", function () {
-        const inputs = {vs1: "67"};
-        const form = {elements: {vs1: {value: ""}}};
-        view.init(form);
-        view.renderInputs(inputs);
-        assert.equal(inputs.vs1, form.elements.vs1.value);
+        const vs1 = form.elements[0];
+        const oldValue = vs1.value;
+        view.init(form, document);
+        vs1.value = "";
+        assert.equal(vs1.value, "");
+        view.renderInputs({vs1: "67"});
+        assert.equal(vs1.value, "67");
+        vs1.value = oldValue;
     });
     it("updates an output", function () {
-        els = {
-            "#vs0": newElement(),
-            "#summary": doc
-        };
-        const vs0 = 57.19;
-        const outputs = {vs0};
+        const vs0 = document.querySelector("#vs0");
+        view.init(form, document);
+        const outputs = {vs0: 57};
         const precision = {vs0: 2};
-        view.init(undefined, doc);
         view.renderOutputs(outputs, precision);
-        assert.equal(els["#vs0"].innerHTML, vs0);
+        assert.equal(vs0.innerHTML, 57.00);
+    });
+    it.skip("Throws and error when result section not found", function () {
+        return;
+    });
+    it.skip("Throws and error when table section not found", function () {
+        return;
     });
     it("updates a result", function () {
         const results = {
-            key1: 3,
-            key2: 4
+            fp: 0.1207,
+            wv2: 45414000
         };
-        const table = [];
-        els = {
-            tbody: {},
-            "#key1": newElement(),
-            "#key2": newElement()
+        const table = [
+            {v: 67, rc: 1177, eta: 0.63, rs: 895, rec: 2560000}
+        ];
+        const precision = {
+            fp: 4,
+            wv2: 0,
+            v: 0,
+            rc: 1,
+            eta: 2,
+            rs: 1,
+            rec: 0
         };
-        const precision = {};
-        view.init(undefined, doc);
+        view.init(form, document);
         view.renderResults({results, table}, precision);
-        assert.equal(els["#key1"].innerHTML, "3");
-        assert.equal(els["#key2"].innerHTML, "4");
+        assert.equal(document.querySelector("#fp").innerHTML, "0.1207");
+        assert.equal(document.querySelector("#wv2").innerHTML, "45414000");
+        assert.equal(
+            document.querySelector("#results tr").innerHTML,
+            "<td>67</td><td>1177.0</td><td>0.63</td>" +
+                    "<td>895.0</td><td>2560000</td>"
+        );
     });
 });
